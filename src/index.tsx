403205f51e7d15057876a80dd02e6c69201fcdcb
llama <possiblellama@gmail.com>
2021-08-18 06:43:50 +0100

    Extract main component into app folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "@app/App";
 
 ReactDOM.render(
-    <div>Worklog</div>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
     document.getElementById("root")
 );
