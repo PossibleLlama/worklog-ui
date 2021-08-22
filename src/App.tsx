c5f32cbc961510902456e6917b192d1dfc20b97e
llama <possiblellama@gmail.com>
2021-08-22 16:01:42 +0100

    Test out windmill components working correctly
    Add svg icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-import { Badge, Card, CardBody } from "@windmill/react-ui";
+import { Badge, Button, Card, CardBody } from "@windmill/react-ui";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import EditIcon from "./Theme/edit.svg";
 
 export const App: React.FC = () => {
     return (
         <Card className="flex">
             <CardBody>
                 <p className="font-semibold text-gray-600 dark:test-gray-300">Worklog</p>
-                <p className="text-gray-600 dark:test-gray-400">I am testing this out.</p>
-                <Badge type="danger">#productivity</Badge>
+                <p className="text-gray-600 dark:test-gray-400">Productivity tool for tracking work done.</p>
+                <div className="flex items-end justify-between">
+                    <div>
+                        <Badge type="primary">#worklog</Badge>
+                    </div>
+                    <div>
+                        <Button size="small" layout="link" aria-label="Like" ><img src={EditIcon} alt="Edit"></img></Button>
+                    </div>
+                </div>
             </CardBody>
         </Card>
     );
