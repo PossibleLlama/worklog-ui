8ac1174ee70f785967c9a346eee5f00277bdc021
llama <possiblellama@gmail.com>
2021-08-26 18:17:32 +0100

    Fix from review

--- README.md
+++ README.md
@@ -2,7 +2,3 @@
 
 UI for the worklog project.
 Allows for easier discovery of trends on what you have been doing.
-
-Icons from [FeatherIcons].
-
-[FeatherIcons]: https://feathericons.com/
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 import { expect } from "chai";
+import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
     beforeEach(() => {
