28cc593ca2abac29f70d08a673b2fbadbd9fe3da
llama <possiblellama@gmail.com>
2021-09-04 12:23:23 +0100

    Remove list and put into timeline
    Add wrapper around app to give background color.
    
    Add timeline components, only exporting abstracted layer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import { Button } from "@zendeskgarden/react-buttons";
-import { Grid, Row, Col } from "@zendeskgarden/react-grid";
-import { Well, Title, Paragraph } from "@zendeskgarden/react-notifications";
-import { Tag } from "@zendeskgarden/react-tags";
+import { Title, Paragraph } from "@zendeskgarden/react-notifications";
 
-import Octicon from "react-component-octicons";
+import Timeline from "@component/Timeline/Timeline.component";
 
-export const App: React.FC = () => {
+import getWorklogByRange from "@api/getWorklogByRange/getWorklogByRange";
+import styled, { ThemeContext } from "styled-components";
+
+const App: React.FC = () => {
+    const theme = useContext(ThemeContext);
+
     return (
-        <Well isRecessed isFloating>
-            <Grid>
-                <Row>
-                    <Title>Worklog</Title>
-                </Row>
-                <Row>
-                    <Paragraph>Productivity tool for tracking work done.</Paragraph>
-                </Row>
-                <Row>
-                    <Col sm={10}>
-                        <Tag isPill>
-                            #worklog
-                            <Tag.Close onClick={() => {
-                                alert("Tag dismissed via mouse");
-                            }} />
-                        </Tag>
-                    </Col>
-                    <Col sm={2}>
-                        <Button>
-                            <Octicon name="pencil" />
-                        </Button>
-                    </Col>
-                </Row>
-            </Grid>
-        </Well>
+        <Wrapper BackgroundColor={theme.background}>
+            <Title>Worklog</Title>
+            <Paragraph>A productivity app.</Paragraph>
+
+            <Timeline worklist={getWorklogByRange(new Date())}/>
+        </Wrapper>
     );
 };
+
+type ThemingProps = {
+    BackgroundColor: string;
+};
+
+const Wrapper = styled.div.attrs((props: ThemingProps) => props)`
+    height: 100%;
+    width: 100%;
+    background-color: ${props => props.BackgroundColor};
+`;
 
 export default App;
---
+++ src/Components/Timeline/Timeline.component.tsx
@@ -0,0 +1,102 @@
+import React, { useContext } from "react";
+
+import styled, { ThemeContext } from "styled-components";
+
+import Preview from "@component/Preview/Preview.component";
+
+import { Work } from "@model/work";
+
+type Props = {
+    worklist: Work[],
+};
+
+export const Timeline: React.FC<Props> = (props: Props) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <Container>
+            <TimelineLine
+                BackgroundColor={theme.primaryHue}
+                BorderColor={theme.primaryHue} />
+            {props.worklist.map((wl) => {
+                return (
+                    <VerticalTimelineElement
+                        key={wl.ID}
+                        Work={wl} />
+                );
+            })}
+        </Container>
+    );
+};
+
+const Container: React.FC = styled.div`
+    margin: 40px 0px;
+
+    display: flex;
+    flex-direction: column;
+    position: relative;
+`;
+
+type ThemingProps = {
+    BackgroundColor: string;
+    BorderColor: string;
+};
+
+const TimelineLine = styled.div.attrs((props: ThemingProps) => props)`
+    position: absolute;
+    left: calc(50% - 2px);
+    width: 4px;
+    height: 100%;
+    background-color: ${props => props.BackgroundColor};
+`;
+
+type VerticalTimelineElementProps = {
+    Work: Work,
+};
+
+const VerticalTimelineElement: React.FC<VerticalTimelineElementProps> = (props: VerticalTimelineElementProps) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <Content>
+            <PreviewWrapper>
+                <Preview work={props.Work} />
+            </PreviewWrapper>
+            <Point
+                BackgroundColor={theme.primaryHue}
+                BorderColor={theme.background} />
+        </Content>
+    );
+};
+
+const Content: React.FC = styled.div`
+    display: flex;
+    position: relative;
+    margin: 10px 0px;
+    width: calc(50% - 10px);
+
+    justify-content: flex-end;
+
+    &:nth-child(odd) {
+        align-self: flex-end;
+        justify-content: flex-start;
+    }
+`;
+
+const PreviewWrapper: React.FC = styled.div`
+    margin: 0 16px;
+`;
+
+const Point = styled.circle.attrs((props: ThemingProps) => props)`
+    margin: 0 -21px;
+    background-color: ${props => props.BackgroundColor};
+    border: 3px solid ${props => props.BorderColor};
+    border-radius: 50%;
+    position: absolute;
+    top: calc(50% - 8px);
+    width: 16px;
+    height: 16px;
+`;
+
+
+export default Timeline;
