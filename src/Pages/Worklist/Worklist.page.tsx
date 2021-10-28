1a29ee57b569e22fe1efda1a9a69a8d1602d40e1
llama <possiblellama@gmail.com>
2021-10-28 13:22:58 +0100

    Remove framer motion

--- src/Pages/Worklist/Worklist.page.tsx
+++ src/Pages/Worklist/Worklist.page.tsx
@@ -1,33 +1,13 @@
 import React, { useState } from "react";
 
-import { AnimatePresence, motion } from "framer-motion";
-
 import Timeline from "@view/Timeline/Timeline.view";
 
-import Details from "@component/Details/Details.component";
+import Modal from "@view/Timeline/DetailsModal.view";
 
 import { Work } from "@model/work";
 
 type Props = {
     Worklist: Work[],
-};
-
-const slideIn = {
-    hidden: {
-        x: "100vw",
-    },
-    visible: {
-        x: "0",
-        transition: {
-            duration: 0.2,
-            type: "spring",
-            damping: 50,
-            stiffness: 500,
-        },
-    },
-    exit: {
-        x: "-100vw",
-    },
 };
 
 const Worklist: React.FC<Props> = (props: Props) => {
@@ -43,22 +23,10 @@ const Worklist: React.FC<Props> = (props: Props) => {
     return (
         <React.Fragment>
             <Timeline Worklist={props.Worklist} onLoadDetailed={loadDetailed}/>
-            
-            <AnimatePresence
-                initial={false}
-                exitBeforeEnter={true}
-            >
-                {detailedWork &&
-                    <motion.div
-                        initial="hidden"
-                        exit="exit"
-                        animate="visible"
-                        variants={slideIn}
-                    >
-                        <Details work={detailedWork} onClose={removeDetailed}/>
-                    </motion.div>
-                }
-            </AnimatePresence>
+
+            {detailedWork &&
+                <Modal detail={detailedWork} onClose={removeDetailed}/>
+            }
         </React.Fragment>
     );
 };
