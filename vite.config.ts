8cbf934f90b64c2c210e57b16a2620975a00b772
llama <possiblellama@gmail.com>
2022-07-20 07:26:23 +0100

    Replace snowpack with vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "./src/API"),
+      "@model": path.resolve(__dirname, "./src/Model"),
+      "@component": path.resolve(__dirname, "./src/Components/"),
+      "@view": path.resolve(__dirname, "./src/Views/"),
+      "@page": path.resolve(__dirname, "./src/Pages/"),
+    },
+  },
+})
