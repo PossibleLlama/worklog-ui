8cf03b125b028c1b8a1f60c09a10edd311ff05e4
llama <possiblellama@gmail.com>
2022-07-25 17:02:01 +0100

    Rotate navbar to fix from top

--- src/Views/Header/Header.view.tsx
+++ src/Views/Header/Header.view.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 import { BookmarkIcon, FilterIcon, GlobeAltIcon } from "@heroicons/react/solid";
 
-import { Button } from "@zendeskgarden/react-buttons";
 import { Alert, Close, useToast } from "@zendeskgarden/react-notifications";
 
 import SidebarIcon from "@component/SidebarIcon/SidebarIcon.component";
@@ -18,7 +17,7 @@ type Props = {
 
 const Header: React.FC<Props> = (props: Props) => {
     const [openModal, setOpenModal] = useState<boolean>(false);
-    
+
     const { addToast } = useToast();
 
     const openFilterModal = (): void => {
@@ -39,20 +38,32 @@ const Header: React.FC<Props> = (props: Props) => {
     };
 
     return (
-        <div className="fixed top-0 left-0 h-screen w-20 flex flex-col bg-stone-100 shadow-lg">
-            <i>
+        <div className="fixed top-0 left-0 w-screen h-16 z-10 bg-stone-100 shadow-lg flex px-2 justify-between items-center">
+            <Link to="/">
                 <h1 className="text-lg text-gray-800">Worklog</h1>
                 <p className="text-sm text-gray-500">A productivity app</p>
-            </i>
-            <SidebarIcon tooltipText="Filter">
-                <FilterIcon />
-            </SidebarIcon>
-            <SidebarIcon tooltipText="Timeline">
-                <BookmarkIcon />
-            </SidebarIcon>
-            <SidebarIcon tooltipText="Discover">
-                <GlobeAltIcon />
-            </SidebarIcon>
+            </Link>
+            <button onClick={openFilterModal}>
+                <SidebarIcon tooltipText="Filter">
+                    <FilterIcon className="h-5/6" />
+                </SidebarIcon>
+            </button>
+            <nav className="flex">
+                <Link to="/timeline" className="px-1">
+                    <button>
+                        <SidebarIcon tooltipText="Timeline">
+                            <BookmarkIcon className="h-5/6" />
+                        </SidebarIcon>
+                    </button>
+                </Link>
+                <Link to="/discover" className="px-1">
+                    <button>
+                        <SidebarIcon tooltipText="Discover">
+                            <GlobeAltIcon className="h-5/6" />
+                        </SidebarIcon>
+                    </button>
+                </Link>
+            </nav>
             {openModal &&
                 <Modal
                     onClose={closeFilterModal}
