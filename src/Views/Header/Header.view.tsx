ee2a6739330f79ab6877dfa10130eb68cab4acf9
llama <possiblellama@gmail.com>
2022-07-25 08:12:47 +0100

    Style header

--- src/Views/Header/Header.view.tsx
+++ src/Views/Header/Header.view.tsx
@@ -6,6 +6,7 @@ import { BookmarkIcon, FilterIcon, GlobeAltIcon } from "@heroicons/react/solid";
 import { Button } from "@zendeskgarden/react-buttons";
 import { Alert, Close, useToast } from "@zendeskgarden/react-notifications";
 
+import SidebarIcon from "@component/SidebarIcon/SidebarIcon.component";
 import Modal from "@view/Filters/FiltersModal.view";
 
 import { Filter, isEqual } from "@model/filter";
@@ -38,41 +39,20 @@ const Header: React.FC<Props> = (props: Props) => {
     };
 
     return (
-        <div className="fixed w-screen top-0 mb-4 flex flex-row border-b-2 border-stone-200 bg-stone-100">
-            <div className="basis-1/3">
-                <nav className="px-2">
-                    <Link to="/">
-                        <h1 className="text-lg text-gray-800">Worklog</h1>
-                        <p className="text-sm text-gray-500">A productivity app</p>
-                    </Link>
-                </nav>
-            </div>
-
-            <div className="basis-1/3">
-                <Button isBasic onClick={openFilterModal}>
-                    <FilterIcon className="h-5 w-5 text-gray-600" />
-                    &nbsp;<h2 className="text-gray-600">Filter</h2>
-                </Button>
-            </div>
-
-            <div className="basis-1/3 flex">
-                <nav className="w-16 mx-2">
-                    <button className="">
-                        <Link to="/timeline">
-                            <BookmarkIcon className="h-5 w-5 text-gray-600"/>
-                            &nbsp;<h2 className="text-gray-600">Timeline</h2>
-                        </Link>
-                    </button>
-                </nav>
-                <nav className="w-16 mx-2">
-                    <button className="">
-                        <Link to="/discover">
-                            <GlobeAltIcon className="h-5 w-5 text-gray-600"/>
-                            &nbsp;<h2 className="text-gray-600">Discover</h2>
-                        </Link>
-                    </button>
-                </nav>
-            </div>
+        <div className="fixed top-0 left-0 h-screen w-20 flex flex-col bg-stone-100 shadow-lg">
+            <i>
+                <h1 className="text-lg text-gray-800">Worklog</h1>
+                <p className="text-sm text-gray-500">A productivity app</p>
+            </i>
+            <SidebarIcon tooltipText="Filter">
+                <FilterIcon />
+            </SidebarIcon>
+            <SidebarIcon tooltipText="Timeline">
+                <BookmarkIcon />
+            </SidebarIcon>
+            <SidebarIcon tooltipText="Discover">
+                <GlobeAltIcon />
+            </SidebarIcon>
             {openModal &&
                 <Modal
                     onClose={closeFilterModal}
