9b586075657edfa3afa58d8add43771ac5516f79
llama <possiblellama@gmail.com>
2022-07-25 17:10:17 +0100

    Make tooltip optional

--- src/Components/SidebarIcon/SidebarIcon.component.tsx
+++ src/Components/SidebarIcon/SidebarIcon.component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type Props = {
-    tooltipText: string,
+    tooltipText?: string,
     children: React.ReactNode,
 };
 
@@ -9,9 +9,9 @@ const SidebarIcon: React.FC<Props> = (props: Props) => {
     return (
         <div className="relative flex items-center justify-center h-12 w-12 m-auto bg-stone-200 hover:bg-stone-600 text-gray-800 hover:text-gray-200 rounded-3xl hover:rounded-xl transition-all ease-linear cursor-default group">
             {props.children}
-            <span className="absolute w-auto p-2 m-2 min-w-max top-12 rounded-md shadow-md bg-stone-600 text-gray-200 text-xs font-bold transition-all duration-100 origin-top scale-0 group-hover:scale-100">
+            {props.tooltipText &&
                 {props.tooltipText}
-            </span>
+                </span>}
         </div>
     );
 };
