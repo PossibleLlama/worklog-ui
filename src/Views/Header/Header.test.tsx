37ae5f33d70b1daa5f2a9134af629bfc3fb0760e
llama <possiblellama@gmail.com>
2022-07-26 17:05:13 +0100

    Brand name as div rather than heading

--- src/Views/Header/Header.test.tsx
+++ src/Views/Header/Header.test.tsx
@@ -10,13 +10,15 @@ import Comp from "./Header.view";
 
 import { Filter } from "@model/filter";
 
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 const updateFilterCallback = jest.fn();
 const startFilter: Filter = {
     startDate: new Date("2000/01/20"),
 };
 
+// Note: tests don't make use of tailwind, so the hover directives won't work
+// This kind of verification should be done at the integration/e2e test level 
 describe("Header", () => {
     beforeEach(() => {
         updateFilterCallback.mockReset();
@@ -32,8 +34,8 @@ describe("Header", () => {
         );
 
         expect(screen.getByText("Worklog"));
-        fireEvent.mouseOver(screen.getByText("Worklog"));
         expect(screen.getByText("Home"));
+        expect(screen.getByRole("button", {name: /Worklog/}));
     });
 
     it("Renders filter", () => {
@@ -45,7 +47,9 @@ describe("Header", () => {
             </BrowserRouter>
         );
 
-        expect(screen.getByText("Filter"));
+        expect(screen.getByRole("button", {name: "Filter"}));
+        // Check the modal itself isn't visible
+        expect(screen.queryByText("Set filters")).toBeNull();
     });
 
     it("Renders nav", () => {
@@ -57,7 +61,7 @@ describe("Header", () => {
             </BrowserRouter>
         );
 
-        expect(screen.getByText("Timeline"));
-        expect(screen.getByText("Discover"));
+        expect(screen.getByRole("button", {name: "Timeline"}));
+        expect(screen.getByRole("button", {name: "Discover"}));
     });
 });
