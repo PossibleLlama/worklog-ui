4ab6f1f03c6402778314496e4ca478b5f5dfc227
llama <possiblellama@gmail.com>
2022-07-26 07:19:54 +0100

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin  } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: "./build"
+  },
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+  ],
   resolve: {
     alias: {
       "@api": path.resolve(__dirname, "./src/API"),
