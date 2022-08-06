d624f6c366a905419c4d7cbc4ed9e6eeefdf9355
llama <possiblellama@gmail.com>
2022-08-06 10:41:44 +0100

    Move margin into header

--- src/Views/Header/Header.view.tsx
+++ src/Views/Header/Header.view.tsx
@@ -30,41 +30,44 @@ const Header: React.FC<Props> = (props: Props) => {
     };
 
     return (
-        <div className="fixed top-0 left-0 w-screen h-16 z-10 bg-stone-100 shadow-lg flex px-2 justify-between items-center">
-            <Link to="/" className="sidebar-item ml-4 px-4 rounded-3xl hover:rounded-xl group">
-                <button className="cursor-default">
-                    <div className="text-lg font-bold">Worklog</div>
-                    <span className="sidebar-tooltip group-hover:scale-100 top-14">
-                        Home
+        <div>
+            <div className="mb-16" />
+            <div className="fixed top-0 left-0 w-screen h-16 z-10 bg-stone-100 shadow-lg flex px-2 justify-between items-center">
+                <Link to="/" className="sidebar-item ml-4 px-4 rounded-3xl hover:rounded-xl group">
+                    <button className="cursor-default">
+                        <div className="text-lg font-bold">Worklog</div>
+                        <span className="sidebar-tooltip group-hover:scale-100 top-14">
+                            Home
                     </span>
+                    </button>
+                </Link>
+                <button onClick={openFilterModal}>
+                    <SidebarIcon tooltipText="Filter">
+                        <FilterIcon className="h-5/6" />
+                    </SidebarIcon>
                 </button>
-            </Link>
-            <button onClick={openFilterModal}>
-                <SidebarIcon tooltipText="Filter">
-                    <FilterIcon className="h-5/6" />
-                </SidebarIcon>
-            </button>
-            <nav className="flex mr-4">
-                <Link to="/timeline" className="px-1">
-                    <button>
-                        <SidebarIcon tooltipText="Timeline">
-                            <BookmarkIcon className="h-5/6" />
-                        </SidebarIcon>
-                    </button>
-                </Link>
-                <Link to="/discover" className="px-1">
-                    <button>
-                        <SidebarIcon tooltipText="Discover">
-                            <GlobeAltIcon className="h-5/6" />
-                        </SidebarIcon>
-                    </button>
-                </Link>
-            </nav>
-            {openModal &&
-                <Modal
-                    onClose={closeFilterModal}
-                    initalFilters={props.currentFilters}
-                />}
+                <nav className="flex mr-4">
+                    <Link to="/timeline" className="px-1">
+                        <button>
+                            <SidebarIcon tooltipText="Timeline">
+                                <BookmarkIcon className="h-5/6" />
+                            </SidebarIcon>
+                        </button>
+                    </Link>
+                    <Link to="/discover" className="px-1">
+                        <button>
+                            <SidebarIcon tooltipText="Discover">
+                                <GlobeAltIcon className="h-5/6" />
+                            </SidebarIcon>
+                        </button>
+                    </Link>
+                </nav>
+                {openModal &&
+                    <Modal
+                        onClose={closeFilterModal}
+                        initalFilters={props.currentFilters}
+                    />}
+            </div>
         </div>
     );
 };
