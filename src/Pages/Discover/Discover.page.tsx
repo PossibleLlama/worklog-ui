0abfda8f693f07d79f50820d4729ded38fb96372
llama <possiblellama@gmail.com>
2022-08-19 12:42:20 +0100

    Use frequency of tags on discovery page

--- src/Pages/Discover/Discover.page.tsx
+++ src/Pages/Discover/Discover.page.tsx
@@ -4,6 +4,7 @@ import Usage from "@view/Usage/Usage.view";
 import Wordcloud from "@view/Wordcloud/Wordcloud.view";
 
 import { Work } from "@model/work";
+import { wordDataFrequencyOfTags } from "@helper/array";
 
 type Props = {
     TotalWork: Work[],
@@ -14,10 +15,7 @@ const Discover: React.FC<Props> = (props: Props) => {
     return (
         <div className="relative justify-center py-4" >
             <Usage {...props} />
-            {/* TODO, update to use frequency of tags */}
-            <Wordcloud words={props.FilteredWork.map((wk) => {
-                return {text: wk.Title, value: wk.Duration ?? 10};
-            })} />
+            <Wordcloud words={wordDataFrequencyOfTags(props.FilteredWork)} />
         </div>
     );
 };
