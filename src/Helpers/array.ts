dfffe004a2f17ea14fd56dc3a935cdb4dc270410
llama <possiblellama@gmail.com>
2022-08-20 07:24:30 +0100

    Update helper to use filter

--- src/Helpers/array.ts
+++ src/Helpers/array.ts
@@ -2,11 +2,13 @@ import { Work } from "@model/work";
 import { WordData } from "@view/Wordcloud/Wordcloud.view";
 
 export const wordDataFrequencyOfTags = (wls: Work[]): WordData[] => {
-    return (wls.map((e) => {
-        if (e.Tags === undefined) {
-            return [];
-        }
-        return e.Tags;
-    }).flat()).reduce((previous: WordData[], current: string) => (
-        (previous[previous.findIndex(d => d.text === current)] || previous[previous.push({ text: current, value: 0 })-1]).value++, previous), []);
+    return wls.filter((e: Work) =>
+        e.Tags !== undefined
+    ).map((e: Work) =>
+        // We've just done a filter to remove undefined values
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        e.Tags!
+    ).flat().reduce((previous: WordData[], current: string) => (
+        (previous[previous.findIndex(d => d.text === current)] || previous[previous.push({ text: current, value: 0 }) - 1]).value++, previous),
+    []);
 };
