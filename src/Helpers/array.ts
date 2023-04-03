5778f7fdc9f102b7d02737be6010466fb00f26dc
Chris Collier <possiblellama@gmail.com>
2023-04-03 19:47:11 +0100

    Only get first 20 elements in word cloud

--- src/Helpers/array.ts
+++ src/Helpers/array.ts
@@ -1,7 +1,11 @@
 import { Work } from "@model/work";
 import { WordData } from "@view/Wordcloud/Wordcloud.view";
 
-export const wordDataFrequencyOfTags = (wls: Work[]): WordData[] => {
+export const wordDataFrequencyOfTags = (wls: Work[], capacity?: number): WordData[] => {
+    if (typeof capacity === "undefined") {
+        capacity = 20;
+    }
+
     return wls.filter((e: Work) =>
         e.Tags !== undefined
     ).map((e: Work) =>
@@ -10,5 +14,5 @@ export const wordDataFrequencyOfTags = (wls: Work[]): WordData[] => {
         e.Tags!
     ).flat().reduce((previous: WordData[], current: string) => (
         (previous[previous.findIndex(d => d.text === current)] || previous[previous.push({ text: current, value: 0 }) - 1]).value++, previous),
-    []);
+    []).sort((a, b) => b.value - a.value).slice(0, capacity);
 };
