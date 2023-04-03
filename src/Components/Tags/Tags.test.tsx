253079b196b8e2d567af0ecc67ff6baa2523e29e
Chris Collier <possiblellama@gmail.com>
2023-04-03 17:55:47 +0100

    Remove cross from tags
    Editing will be via a modal, dont need an additional way to edit tags specifically.

--- src/Components/Tags/Tags.test.tsx
+++ src/Components/Tags/Tags.test.tsx
@@ -5,17 +5,11 @@ import React from "react";
 
 import Comp from "./Tags.component";
 
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 describe("Tags", () => {
-    const mockClick = jest.fn();
-
-    beforeEach(() => {
-        mockClick.mockReset();
-    });
-
-    it("Renders without close button", () => {
+    it("Renders", () => {
         render(
             <Comp>
                 foo
@@ -23,33 +17,5 @@ describe("Tags", () => {
         );
 
         expect(screen.getByText("foo")).toBeInTheDocument();
-        expect(screen.queryByLabelText("Remove", { selector: "button" })).toBeNull();
-        expect(mockClick).not.toHaveBeenCalled();
-    });
-
-    it("Renders with close button", () => {
-        render(
-            <Comp onClose={mockClick}>
-                foo
-            </Comp>
-        );
-
-        expect(screen.getByText("foo")).toBeInTheDocument();
-        expect(screen.getByLabelText("Remove", { selector: "button" })).toBeInTheDocument();
-        expect(mockClick).not.toHaveBeenCalled();
-    });
-
-    it("Button calls function", () => {
-        render(
-            <Comp onClose={mockClick}>
-                foo
-            </Comp>
-        );
-
-        expect(screen.getByLabelText("Remove", { selector: "button" })).toBeInTheDocument();
-        expect(mockClick).not.toHaveBeenCalled();
-        fireEvent.click(screen.getByLabelText("Remove", { selector: "button" }));
-        expect(mockClick).toHaveBeenCalled();
-        expect(mockClick).toHaveBeenCalledTimes(1);
     });
 });
