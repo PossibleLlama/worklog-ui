e44d99655bc1d01c1e7f9de5dec2c2f1ffaea16a
Chris Collier <possiblellama@gmail.com>
2023-04-06 08:39:50 +0100

    Sort final colours

--- src/Views/Usage/Usage.view.tsx
+++ src/Views/Usage/Usage.view.tsx
@@ -11,8 +11,8 @@ import { isAfter } from "@helper/date";
 
 const innerContainerStyling = "mx-2 p-2 bg-opacity-40 rounded-lg shadow-lg colour-bg-primary-light-no-hover";
 const infographicStyling = "mx-2 p-2 rounded-lg colour-bg-primary-light-no-hover";
-const positiveArrowStyling = "h-5 w-5 mx-1 text-green-500 hover:text-green-300 rounded-3xl flip-flop";
-const negativeArrowStyling = "h-5 w-5 mx-1 text-red-600 hover:text-red-300 rounded-3xl flip-flop";
+const positiveArrowStyling = "h-5 w-5 mx-1 rounded-3xl colour-text-positive flip-flop";
+const negativeArrowStyling = "h-5 w-5 mx-1 rounded-3xl colour-text-negative flip-flop";
 
 const averagePerDay = (wk: Work[]): number => {
     const uniqueDays = [... new Set(wk.map((e) => {
